95c21d8c35da6789a705a18e5ccf64c15be6f35e
Miguel Angel Cañas Vaz <macvaz82@gmail.com>
2023-08-29 21:45:50 +0200

    Added missing files

---
+++ docker/etl-tools/scripts/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export JAVA_HOME="$(jrunscript -e 'java.lang.System.out.println(java.lang.System.getProperty("java.home"));')"
+
+# spark
+mkdir -p /tmp/spark-events
+start-master.sh -p 7077 --webui-port 8061
+start-worker.sh spark://spark:7077 --webui-port 8062
+start-history-server.sh
+
+# Entrypoint, for example notebook, pyspark or spark-sql
+if [[ $# -gt 0 ]] ; then
+    eval "$1"
+fi
